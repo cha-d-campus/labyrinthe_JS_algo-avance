9b54506b638ac618d8c2c9ef2ad5a0a60995f761
Cha <cd.webdesign.graphism@gmail.com>
2022-09-01 17:09:25 +0200

    resolve labyrinthe

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Variable donnant la taille en case du labyrinthe 
+let size_lab = 15;
+
+// Variable donnant le numéro d'exemple de labyrinthe choisi
+let ex_lab = 0;
+
+// Variable récupérant les labyrinthes
+const lab = labyrinthes[size_lab.toString()]['ex-' + ex_lab.toString()];
+
 // Création d'une case du labyrinthe
 function createCell(dataCell) {
     const cell = document.createElement("div");
@@ -10,9 +19,20 @@ function createCell(dataCell) {
     cell.style.borderColor = "blueviolet";
     cell.style.borderStyle = "solid";
     cell.style.backgroundColor = createStartEnd(dataCell["entrance"], dataCell["exit"])
-    cell.visited = false;
     return cell;
 };
+
+function buildPath(currentCell) {
+    while (currentCell.parent) {
+        pathCell = currentCell.parent;
+        document.getElementById(`${pathCell.posX}-${pathCell.posY}`).style.backgroundColor = "purple"
+        console.log("chemin :")
+        console.log(pathCell);
+    }
+    return
+}
+
+
 
 // Création des murs du labyrinthe
 function createBorder(walls) {
@@ -32,8 +52,6 @@ function createStartEnd(start, end) {
     return colorBackground;
 };
 
-
-
 // Création du labyrinthe
 function createLabyrinthe(data) {
     let size = sizeContainer(lab);
@@ -44,24 +62,52 @@ function createLabyrinthe(data) {
         let cell = createCell(cellData);
         container.appendChild(cell);
     });
-    
 };
 
-function getCell(x, y){
-    let neighbor = 
-    return neighbor;
+// Fonction qui récupère la case d'entrée
+lab.getStart = function () {
+    let startCell = this.find(cell => cell.entrance);
+    return startCell;
 }
 
-function getNeighbors(cell) {
-    let neighbors = []; 
-    if (cell.walls[0] )
+// Fonction qui récupère les coordonnées de la case courante
+lab.getCell = function (x, y) {
+    let currentCell = this.find(cell => cell.posX == x && cell.posY == y);
+    return currentCell;
+};
+
+// Fonction qui trouve les cases voisines à la case courante et qui sont accessibles 
+lab.getNeighbors = function (cell) {
+    let neighbors = [];
+    let topNeighbor = this.getCell(cell.posX - 1, cell.posY);
+    let rightNeighbor = this.getCell(cell.posX, cell.posY + 1);
+    let bottomNeighbor = this.getCell(cell.posX + 1, cell.posY);
+    let letfNeighbor = this.getCell(cell.posX, cell.posY - 1);
+    if (topNeighbor && !cell.walls[0]) {
+        neighbors.push(topNeighbor)
+    };
+    if (rightNeighbor && !cell.walls[1]) {
+        neighbors.push(rightNeighbor)
+    };
+    if (bottomNeighbor && !cell.walls[2]) {
+        neighbors.push(bottomNeighbor)
+    };
+    if (letfNeighbor && !cell.walls[3]) {
+        neighbors.push(letfNeighbor)
+    };
+    return neighbors;
 }
 
 // Création du Stack
 function Stack() {
-    this.push = (el) => { console.log("pushing" + el) };
-    this.isEmpty = () => true;
-    this.pop = () => { console.log("poping") }
+    this.data = [];
+    this.isEmpty = () => this.data.length == 0;
+    this.push = (elt) => {
+        if (typeof elt !== "undefined") {
+            this.data.push(elt);
+        }
+    }
+    this.pop = () => this.data.pop();
 }
 
 // Création de la fonction de jeu pour trouver le chemin de la case de départ à la case de sortie du labyrinthe
@@ -70,16 +116,23 @@ function resolveLab(lab, start) {
     waitingList.push(start);
     while (!waitingList.isEmpty()) {
         let currentCell = waitingList.pop();
+
         if (!currentCell.visited) {
-            currentCell.visited === true;
-            if (currentCell.isExit) {
-                return labPath(start, currentCell);
+            currentCell.visited = true;
+
+            document.getElementById(`${currentCell.posX}-${currentCell.posY}`).style.backgroundColor = "purple"
+
+
+            console.log(currentCell)
+            if (currentCell.exit) {
+                return buildPath(currentCell);
             }
             lab.getNeighbors(currentCell).forEach(neighborCell => {
                 if (!neighborCell.visited) {
-                    neighborCell.parentOf(currentCell);
-                    neighborCell.pop();
+                    neighborCell.parent = currentCell;
+                    waitingList.push(currentCell);
                 }
+                waitingList.push(neighborCell)
             });
         }
     }
@@ -99,15 +152,9 @@ function getId(x, y) {
     return id
 };
 
-// Variable donnant la taille en case du labyrinthe 
-let size_lab = 12;
 
-// Variable donnant le numéro d'exemple de labyrinthe choisi
-let ex_lab = 1;
-
-// Variable récupérant les labyrinthes
-const lab = labyrinthes[size_lab.toString()]['ex-' + ex_lab.toString()];
 
 // Appel de la function qui créée le labyrinthe
 createLabyrinthe(lab)
-resolveLab(lab);
+resolveLab(lab, lab.getStart());
+
